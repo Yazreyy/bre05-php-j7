1a58a7d91d2920f46ecfdf26e37e422d6606d8aa
Marlonn Gillet <gillet.marlonn56@icloud.com>
2026-02-19 13:38:15 +0100

    exercice session

---
+++ session/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Choix du pseudo</title>
+</head>
+<body>
+<form method="POST" action="nickname.php">
+<label for="pseudo">Pseudo :</label>
+    <input type="text" name="pseudo" id="pseudo">
+    <button type="submit">Valider</button>
+    <a href="logout.php">Déconnexion</a>
+</form>
+</body>
+</html>
---
+++ session/nickname.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+
+if(isset($_POST['pseudo'])){
+$pseudo = $_POST['pseudo'];
+$_SESSION ['pseudo'] = $pseudo;
+}
+?>
